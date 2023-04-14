c443ef9b6c1a803379280b3212f29898f2696dd1
subin <lsb1026@naver.com>
2023-04-14 10:24:24 +0900

    feat: 뱃지 엔티티 생성

---
+++ src/main/java/esubine/community/badge/BadgeEntity.java
@@ -0,0 +1,63 @@
+package esubine.community.badge;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "badge")
+@EntityListeners(AuditingEntityListener.class)
+public class BadgeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "badge_id")
+    private Long badgeId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Nullable
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "description")
+    private String description;
+
+    @CreatedDate
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @LastModifiedDate
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+
+    @Nullable
+    @Column(name = "board_count")
+    private int boardCount;
+
+    @Nullable
+    @Column(name = "comment_count")
+    private int commentCount;
+
+    @Nullable
+    @Column(name = "like_count")
+    private int likeCount;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/esubine/community/badge/BadgeRepository.java
@@ -0,0 +1,6 @@
+package esubine.community.badge;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BadgeRepository extends JpaRepository<BadgeEntity, Long> {
+}
