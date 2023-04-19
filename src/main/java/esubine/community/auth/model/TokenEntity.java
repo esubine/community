b8969db9b30edb9d199ba255fa94af256a2c1639
subin <lsb1026@naver.com>
2023-04-19 17:41:48 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/main/java/esubine/community/auth/model/TokenEntity.java
+++ src/main/java/esubine/community/auth/model/TokenEntity.java
@@ -3,10 +3,8 @@ package esubine.community.auth.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @Entity
 @Table(name="token")
